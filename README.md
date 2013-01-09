11479d2fb2abd60dbe0107734b2d8310a8db4e0e
wjw465150 <wjw465150@gmail.com>
2013-01-09 15:41:38 +0800

    wjw first add to git

---
+++ README.md
@@ -0,0 +1,5 @@
+Pro-Git-Chinese
+===============
+
+带菜单导航的Git中文指南,Git很好奥,我只是加了个导航菜单,方便网友查到,学习! 由于OSChian限制附件大小为4M,请到:https://wjw465150@github.com/wjw465150/Pro-Git-Chinese.git下载
+注意:开始页面是:__index.html
---
+++ menus.js
@@ -0,0 +1,504 @@
+var wjw_tree_menus=
+{
+  "identifier": "id", 
+  "label": "title", 
+  "items": [
+    {
+      "id": 0, 
+      "title": "Git", 
+      "url": "index.html", 
+      "items": [
+        {
+          "id": 1, 
+          "title": "1. 起步", 
+          "url": "ch1-0.html", 
+          "items": [
+            {
+              "id": 2, 
+              "title": "1.1 - 关于版本控制", 
+              "url": "ch1-1.html", 
+              "items": [
+              ]
+            }, 
+            {
+              "id": 3, 
+              "title": "1.2 - Git 简史", 
+              "url": "ch1-2.html", 
+              "items": [
+              ]
+            }, 
+            {
+              "id": 4, 
+              "title": "1.3 - Git 基础", 
+              "url": "ch1-3.html", 
+              "items": [
+              ]
+            }, 
+            {
+              "id": 5, 
+              "title": "1.4 - 安装 Git", 
+              "url": "ch1-4.html", 
+              "items": [
+              ]
+            }, 
+            {
+              "id": 6, 
+              "title": "1.5 - 初次运行 Git 前的配置", 
+              "url": "ch1-5.html", 
+              "items": [
+              ]
+            }, 
+            {
+              "id": 7, 
+              "title": "1.6 - 获取帮助", 
+              "url": "ch1-6.html", 
+              "items": [
+              ]
+            }, 
+            {
+              "id": 8, 
+              "title": "1.7 - 小结", 
+              "url": "ch1-7.html", 
+              "items": [
+              ]
+            }
+          ]
+        }, 
+        {
+          "id": 9, 
+          "title": "2. Git 基础", 
+          "url": "ch2-0.html", 
+          "items": [
+            {
+              "id": 10, 
+              "title": "2.1 - 取得项目的 Git 仓库", 
+              "url": "ch2-1.html", 
+              "items": [
+              ]
+            }, 
+            {
+              "id": 11, 
+              "title": "2.2 - 记录每次更新到仓库", 
+              "url": "ch2-2.html", 
+              "items": [
+              ]
+            }, 
+            {
+              "id": 12, 
+              "title": "2.3 - 查看提交历史", 
+              "url": "ch2-3.html", 
+              "items": [
+              ]
+            }, 
+            {
+              "id": 13, 
+              "title": "2.4 - 撤消操作", 
+              "url": "ch2-4.html", 
+              "items": [
+              ]
+            }, 
+            {
+              "id": 14, 
+              "title": "2.5 - 远程仓库的使用", 
+              "url": "ch2-5.html", 
+              "items": [
+              ]
+            }, 
+            {
+              "id": 15, 
+              "title": "2.6 - 打标签", 
+              "url": "ch2-6.html", 
+              "items": [
+              ]
+            }, 
+            {
+              "id": 16, 
+              "title": "2.7 - 技巧和窍门", 
+              "url": "ch2-7.html", 
+              "items": [
+              ]
+            }, 
+            {
+              "id": 17, 
+              "title": "2.8 - 小结", 
+              "url": "ch2-8.html", 
+              "items": [
+              ]
+            }
+          ]
+        }, 
+        {
+          "id": 18, 
+          "title": "3. Git 分支", 
+          "url": "ch3-0.html", 
+          "items": [
+            {
+              "id": 19, 
+              "title": "3.1 - 何谓分支", 
+              "url": "ch3-1.html", 
+              "items": [
+              ]
+            }, 
+            {
+              "id": 20, 
+              "title": "3.2 - 分支的新建与合并", 
+              "url": "ch3-2.html", 
+              "items": [
+              ]
+            }, 
+            {
+              "id": 21, 
+              "title": "3.3 - 分支的管理", 
+              "url": "ch3-3.html", 
+              "items": [
+              ]
+            }, 
+            {
+              "id": 22, 
+              "title": "3.4 - 利用分支进行开发的工作流程", 
+              "url": "ch3-4.html", 
+              "items": [
+              ]
+            }, 
+            {
+              "id": 23, 
+              "title": "3.5 - 远程分支", 
+              "url": "ch3-5.html", 
+              "items": [
+              ]
+            }, 
+            {
+              "id": 24, 
+              "title": "3.6 - 分支的衍合", 
+              "url": "ch3-6.html", 
+              "items": [
+              ]
+            }, 
+            {
+              "id": 25, 
+              "title": "3.7 - 小结", 
+              "url": "ch3-7.html", 
+              "items": [
+              ]
+            }
+          ]
+        }, 
+        {
+          "id": 26, 
+          "title": "4. 服务器上的 Git", 
+          "url": "ch4-0.html", 
+          "items": [
+            {
+              "id": 27, 
+              "title": "4.1 - 协议", 
+              "url": "ch4-1.html", 
+              "items": [
+              ]
+            }, 
+            {
+              "id": 28, 
+              "title": "4.2 - 在服务器上部署 Git", 
+              "url": "ch4-2.html", 
+              "items": [
+              ]
+            }, 
+            {
+              "id": 29, 
+              "title": "4.3 - 生成 SSH 公钥", 
+              "url": "ch4-3.html", 
+              "items": [
+              ]
+            }, 
+            {
+              "id": 30, 
+              "title": "4.4 - 架设服务器", 
+              "url": "ch4-4.html", 
+              "items": [
+              ]
+            }, 
+            {
+              "id": 31, 
+              "title": "4.5 - 公共访问", 
+              "url": "ch4-5.html", 
+              "items": [
+              ]
+            }, 
+            {
+              "id": 32, 
+              "title": "4.6 - GitWeb", 
+              "url": "ch4-6.html", 
+              "items": [
+              ]
+            }, 
+            {
+              "id": 33, 
+              "title": "4.7 - Gitosis", 
+              "url": "ch4-7.html", 
+              "items": [
+              ]
+            }, 
+            {
+              "id": 34, 
+              "title": "4.8 - Gitolite", 
+              "url": "ch4-8.html", 
+              "items": [
+              ]
+            }, 
+            {
+              "id": 35, 
+              "title": "4.9 - Git 守护进程", 
+              "url": "ch4-9.html", 
+              "items": [
+              ]
+            }, 
+            {
+              "id": 36, 
+              "title": "4.10 - Git 托管服务", 
+              "url": "ch4-10.html", 
+              "items": [
+              ]
+            }, 
+            {
+              "id": 37, 
+              "title": "4.11 - 小结", 
+              "url": "ch4-11.html", 
+              "items": [
+              ]
+            }
+          ]
+        }, 
+        {
+          "id": 38, 
+          "title": "5. 分布式 Git", 
+          "url": "ch5-0.html", 
+          "items": [
+            {
+              "id": 39, 
+              "title": "5.1 - 分布式工作流程", 
+              "url": "ch5-1.html", 
+              "items": [
+              ]
+            }, 
+            {
+              "id": 40, 
+              "title": "5.2 - 为项目作贡献", 
+              "url": "ch5-2.html", 
+              "items": [
+              ]
+            }, 
+            {
+              "id": 41, 
+              "title": "5.3 - 项目的管理", 
+              "url": "ch5-3.html", 
+              "items": [
+              ]
+            }, 
+            {
+              "id": 42, 
+              "title": "5.4 - 小结", 
+              "url": "ch5-4.html", 
+              "items": [
+              ]
+            }
+          ]
+        }, 
+        {
+          "id": 43, 
+          "title": "6. Git 工具", 
+          "url": "ch6-0.html", 
+          "items": [
+            {
+              "id": 44, 
+              "title": "6.1 - 修订版本（Revision）选择", 
+              "url": "ch6-1.html", 
+              "items": [
+              ]
+            }, 
+            {
+              "id": 45, 
+              "title": "6.2 - 交互式暂存", 
+              "url": "ch6-2.html", 
+              "items": [
+              ]
+            }, 
+            {
+              "id": 46, 
+              "title": "6.3 - 储藏（Stashing）", 
+              "url": "ch6-3.html", 
+              "items": [
+              ]
+            }, 
+            {
+              "id": 47, 
+              "title": "6.4 - 重写历史", 
+              "url": "ch6-4.html", 
+              "items": [
+              ]
+            }, 
+            {
+              "id": 48, 
+              "title": "6.5 - 使用 Git 调试", 
+              "url": "ch6-5.html", 
+              "items": [
+              ]
+            }, 
+            {
+              "id": 49, 
+              "title": "6.6 - 子模块", 
+              "url": "ch6-6.html", 
+              "items": [
+              ]
+            }, 
+            {
+              "id": 50, 
+              "title": "6.7 - 子树合并", 
+              "url": "ch6-7.html", 
+              "items": [
+              ]
+            }, 
+            {
+              "id": 51, 
+              "title": "6.8 - 总结", 
+              "url": "ch6-8.html", 
+              "items": [
+              ]
+            }
+          ]
+        }, 
+        {
+          "id": 52, 
+          "title": "7. 自定义 Git", 
+          "url": "ch7-0.html", 
+          "items": [
+            {
+              "id": 53, 
+              "title": "7.1 - 配置 Git", 
+              "url": "ch7-1.html", 
+              "items": [
+              ]
+            }, 
+            {
+              "id": 54, 
+              "title": "7.2 - Git属性", 
+              "url": "ch7-2.html", 
+              "items": [
+              ]
+            }, 
+            {
+              "id": 55, 
+              "title": "7.3 - Git挂钩", 
+              "url": "ch7-3.html", 
+              "items": [
+              ]
+            }, 
+            {
+              "id": 56, 
+              "title": "7.4 - Git 强制策略实例", 
+              "url": "ch7-4.html", 
+              "items": [
+              ]
+            }, 
+            {
+              "id": 57, 
+              "title": "7.5 - 总结", 
+              "url": "ch7-5.html", 
+              "items": [
+              ]
+            }
+          ]
+        }, 
+        {
+          "id": 58, 
+          "title": "8. Git 与其他系统", 
+          "url": "ch8-0.html", 
+          "items": [
+            {
+              "id": 59, 
+              "title": "8.1 - Git 与 Subversion", 
+              "url": "ch8-1.html", 
+              "items": [
+              ]
+            }, 
+            {
+              "id": 60, 
+              "title": "8.2 - 迁移到 Git", 
+              "url": "ch8-2.html", 
+              "items": [
+              ]
+            }, 
+            {
+              "id": 61, 
+              "title": "8.3 - 总结", 
+              "url": "ch8-3.html", 
+              "items": [
+              ]
+            }
+          ]
+        }, 
+        {
+          "id": 62, 
+          "title": "9. Git 内部原理", 
+          "url": "ch9-0.html", 
+          "items": [
+            {
+              "id": 63, 
+              "title": "9.1 - 底层命令 (Plumbing) 和高层命令 (Porcelain)", 
+              "url": "ch9-1.html", 
+              "items": [
+              ]
+            }, 
+            {
+              "id": 64, 
+              "title": "9.2 - Git 对象", 
+              "url": "ch9-2.html", 
+              "items": [
+              ]
+            }, 
+            {
+              "id": 65, 
+              "title": "9.3 - Git References", 
+              "url": "ch9-3.html", 
+              "items": [
+              ]
+            }, 
+            {
+              "id": 66, 
+              "title": "9.4 - Packfiles", 
+              "url": "ch9-4.html", 
+              "items": [
+              ]
+            }, 
+            {
+              "id": 67, 
+              "title": "9.5 - The Refspec", 
+              "url": "ch9-5.html", 
+              "items": [
+              ]
+            }, 
+            {
+              "id": 68, 
+              "title": "9.6 - 传输协议", 
+              "url": "ch9-6.html", 
+              "items": [
+              ]
+            }, 
+            {
+              "id": 69, 
+              "title": "9.7 - 维护及数据恢复", 
+              "url": "ch9-7.html", 
+              "items": [
+              ]
+            }, 
+            {
+              "id": 70, 
+              "title": "9.8 - 总结", 
+              "url": "ch9-8.html", 
+              "items": [
+              ]
+            }
+          ]
+        }
+      ]
+    }
+  ]
+}
